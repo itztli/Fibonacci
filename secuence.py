4518c5eb0af171a530821729de73312fc1601f4e
Victor De la Luz <vdelaluz@enesmorelia.unam.mx>
2023-10-11 12:13:30 -0600

    my first sequence

---
+++ secuence.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#By vdelaluz@enesmorelia.unam.mx
+# GNU/GPL License
+# My first secuence
+
+# sumar los 10 primeros numeros enteros
+
+suma = 1+2+3+4+5+6+7+8+9+10
+
+print(suma)
